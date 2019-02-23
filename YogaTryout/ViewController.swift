629d6b72e355df71e3088cff76af957dec580947
Hai Vu <haivu@Hais-MacBook-Pro.local>
2019-02-23 17:39:42 +0700

    Initial Commit

---
+++ YogaTryout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YogaTryout
+//
+//  Created by Hai Vu on 2/23/19.
+//  Copyright © 2019 Hai Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
